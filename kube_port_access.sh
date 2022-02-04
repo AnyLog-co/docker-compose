5137b188d67e058506db8be01b43e73c0f38c709
Ori Shadmon <oshadmon@gmail.com>
2022-02-04 21:57:44 +0000

    test

--- kube_port_access.sh
+++ kube_port_access.sh
@@ -1,13 +1,2 @@
-#!/usr/bin/env bash
-if [[ $# -eq 1 ]]
-then
-  IPADDR=$1
-else
-  echo "Missing IP Address"
-  exit 1
-fi
-
-kubectl port-forward --address=${IPADDR} service/grafana 3000:3000
-kubectl port-forward --address=${IPADDR} service/database 5432:5432
-kubectl port-forward --address=${IPADDR} service/remote-cli 8000:8000
-kubectl port-forward --address=${IPADDR} service/anylog-gui 5000:5000
+IPADDR=10.0.0.91
+kubectl port-forward --address 10.0.0.91 service/remote-cli 8000:8000
