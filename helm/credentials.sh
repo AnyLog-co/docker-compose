fc412eab8023bf89887f42ae530dc42d687fe345
Oi Shadmon <oshadmon@gmail.com>
2022-03-08 18:34:26 -0800

    rewrite k8s

--- helm/credentials.sh
+++ helm/credentials.sh
@@ -10,7 +10,7 @@ fi
 DOCKER_REGISTRY_SERVER=docker.io
 DOCKER_USER=oshadmon
 DOCKER_EMAIL=ori@anylog.co
-kubectl create secret docker-registry anylogregistrykey \
+kubectl create secret docker-registry anylog-registry-key \
   --docker-server=${DOCKER_REGISTRY_SERVER} \
   --docker-username=${DOCKER_USER} \
   --docker-password=${DOCKER_PASSWORD} \
