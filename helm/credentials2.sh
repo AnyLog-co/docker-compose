9696c57659266d4c31990b5caa61f3c3d403801e
Ori Shadmon <oshadmon@gmail.com>
2022-04-19 17:27:37 +0000

    nvidia only

--- helm/credentials2.sh
+++
@@ -1,8 +0,0 @@
-# Set docker credentials for Kubernetes & Helm
-DOCKER_PASSWORD=ZHZ1b3EyNWdlcG5kNXJzZHBzbXVoM2Y5ZmI6ZDVmMzQ0MGItMGE2ZS00YTRkLWFmYjEtYTU1NTlmN2FhZDgx
-DOCKER_REGISTRY_SERVER=nvcr.io
-DOCKER_USER=\$oauthtoken 
-kubectl create secret docker-registry anylog-registry-key \
-  --docker-server=${DOCKER_REGISTRY_SERVER} \
-  --docker-username=${DOCKER_USER} \
-  --docker-password=${DOCKER_PASSWORD} \
