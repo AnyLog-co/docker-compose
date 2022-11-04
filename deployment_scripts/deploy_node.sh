c2b18dc1d53e3a38101162435a39e15e53134a00
Ori Shadmon <oshadmon@gmail.com>
2022-11-04 09:48:32 -0700

    master / ledger terminology

--- deployment_scripts/deploy_node.sh
+++ deployment_scripts/deploy_node.sh
@@ -3,23 +3,23 @@
 The following code initiates a Python3 script that helps setup configurations for an AnyLog instance.
 COMMENT
 
-read -p "Node Type [default: rest | options: rest, mastaer, operator, publisher, query, info]: " NODE_TYPE
-while [[ ! ${NODE_TYPE}  == rest ]] && [[ ! ${NODE_TYPE}  == mastaer ]] && [[ ! ${NODE_TYPE}  == operator ]] && [[ ! ${NODE_TYPE}  == publisher ]] && [[ ! ${NODE_TYPE}  == query ]] && [[ ! -z ${NODE_TYPE} ]] ;
+read -p "Node Type [default: rest | options: rest, master, operator, publisher, query, info]: " NODE_TYPE
+while [[ ! ${NODE_TYPE}  == rest ]] && [[ ! ${NODE_TYPE}  == master ]] && [[ ! ${NODE_TYPE}  == operator ]] && [[ ! ${NODE_TYPE}  == publisher ]] && [[ ! ${NODE_TYPE}  == query ]] && [[ ! -z ${NODE_TYPE} ]] ;
 do
   if [[ ${NODE_TYPE}  == info ]] ;
   then
     printf 'Node type options to deploy: '
 #    printf '\n\none - an AnyLog instance with literally nothing configured'
     printf '\n\trest - sandbox for understanding AnyLog as only TCP & REST are configured'
-    printf '\n\tmastaer - a database node replacing an actual blockchain'
+    printf '\n\tmaster - a database node replacing an actual blockchain'
     printf '\n\toperator - node where data will be stored'
     printf '\n\tpublisher - node to distribute data among operators'
-    printf '\n\tquery - node dedicated to mastaer node (installed with Remote-CLI)\n'
-#    printf "\n\tstandalone - node that consists of both mastaer and operator deployment configurations"
-#    printf "\n\tstandalone-publisher - node that consists of both mastaer and publisher deployment configurations"
-    read -p "Node Type [default: rest | options: rest, mastaer, operator, publisher, query, info]: " NODE_TYPE
+    printf '\n\tquery - node dedicated to master node (installed with Remote-CLI)\n'
+#    printf "\n\tstandalone - node that consists of both master and operator deployment configurations"
+#    printf "\n\tstandalone-publisher - node that consists of both master and publisher deployment configurations"
+    read -p "Node Type [default: rest | options: rest, master, operator, publisher, query, info]: " NODE_TYPE
   else
-    read -p "Invalid node type '${NODE_TYPE}'. Node Type [default: rest | options: rest, mastaer, operator, publisher, query, info]: " NODE_TYPE
+    read -p "Invalid node type '${NODE_TYPE}'. Node Type [default: rest | options: rest, master, operator, publisher, query, info]: " NODE_TYPE
   fi
 done
 if [[ -z ${NODE_TYPE} ]] ; then NODE_TYPE=rest ; fi
