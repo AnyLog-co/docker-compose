6aa44855fb2edf8745b2768df20c6a126f4556b3
Ori Shadmon <oshadmon@gmail.com>
2022-11-22 09:41:55 -0800

    pull from os-dev

---
+++ deployment_scripts/deploy_node.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+<<COMMENT
+The following code initiates a Python3 script that helps setup configurations for an AnyLog instance.
+COMMENT
+
+read -p "Node Type [default: rest | options: rest, master, operator, publisher, query, info]: " NODE_TYPE
+while [[ ! ${NODE_TYPE}  == rest ]] && [[ ! ${NODE_TYPE}  == master ]] && [[ ! ${NODE_TYPE}  == operator ]] && [[ ! ${NODE_TYPE}  == publisher ]] && [[ ! ${NODE_TYPE}  == query ]] && [[ ! -z ${NODE_TYPE} ]] ;
+do
+  if [[ ${NODE_TYPE}  == info ]] ;
+  then
+    printf 'Node type options to deploy: '
+#    printf '\n\none - an AnyLog instance with literally nothing configured'
+    printf '\n\trest - sandbox for understanding AnyLog as only TCP & REST are configured'
+    printf '\n\tmaster - a database node replacing an actual blockchain'
+    printf '\n\toperator - node where data will be stored'
+    printf '\n\tpublisher - node to distribute data among operators'
+    printf '\n\tquery - node dedicated to master node (installed with Remote-CLI)\n'
+#    printf "\n\tstandalone - node that consists of both master and operator deployment configurations"
+#    printf "\n\tstandalone-publisher - node that consists of both master and publisher deployment configurations"
+    read -p "Node Type [default: rest | options: rest, master, operator, publisher, query, info]: " NODE_TYPE
+  else
+    read -p "Invalid node type '${NODE_TYPE}'. Node Type [default: rest | options: rest, master, operator, publisher, query, info]: " NODE_TYPE
+  fi
+done
+if [[ -z ${NODE_TYPE} ]] ; then NODE_TYPE=rest ; fi
+
+
+read -p "Deploy Existing Configs [y / n]: " EXISTING_CONFIGS
+while [[ ! ${EXISTING_CONFIGS} == y ]] && [[ ! ${EXISTING_CONFIGS} == n ]] ;
+do
+  read -p "Invalid option: ${EXISTING_CONFIGS}. Deploy Existing Configs [y / n]: " EXISTING_CONFIGS
+done
+
+# if user decides not to use existing configs, then ask questions to help fill-out the configurations.
+if [[ ${EXISTING_CONFIGS} == n ]] ;
+then
+  read -p "AnyLog Build Version [default: develop | options: develop, predevelop, test]: " BUILD_TYPE
+  while [[ ! ${BUILD_TYPE} == develop ]] && [[ ! ${BUILD_TYPE} == predevelop ]] && [[ ! ${BUILD_TYPE} == test ]]  && [[ ! -z ${BUILD_TYPE} ]] ;
+  do
+    read -p "Invalid build type: ${BUILD_TYPE}. AnyLog Build Version [default: develop | options: develop, predevelop, test]: " BUILD_TYPE
+  done
+  if [[ -z ${BUILD_TYPE} ]] ; then BUILD_TYPE=develop ; fi
+  printf "\n"
+
+  python3 $HOME/deployments/deployment_scripts/main.py ${NODE_TYPE} \
+    --build ${BUILD_TYPE} \
+    --deployment-type docker \
+    --config-file $HOME/deployments/deployment_scripts/configurations.json
+fi
+
+read -p  "Would you like to deploy AnyLog now (y/n)?" DEPLOY_NODE
+if [[ ! ${DEPLOY_NODE} == "n" ]];
+then
+  if [[ ! ${NODE_TYPE} == query ]] ;
+  then
+    cd $HOME/deployments/docker-compose/anylog-${NODE_TYPE}
+  else
+    cd $HOME/deployments/docker-compose/query-remote-cli/
+  fi
+  docker-compose up -d
+else
+  if [[ ! ${NODE_TYPE} == query ]] ;
+  then
+    NODE_PATH=$HOME/deployments/docker-compose/anylog-${NODE_TYPE}
+  else
+    NODE_PATH=$HOME/deployments/docker-compose/query-remote-cli
+  fi
+  echo "Node was not deployed. Configurations were saved in: ${NODE_PATH}"
+fi
+
---
+++ deployment_scripts/support.py
@@ -0,0 +1,114 @@
+import json 
+import os 
+
+
+def json_read_file(file_name:str) -> dict:
+    """
+    Read JSON file 
+    :args:
+        file_name:str - file to read content from 
+    :params: 
+        content:dict - content from file 
+    :return: 
+        content
+    """
+    content = {}
+    full_path = os.path.expandvars(os.path.expanduser(file_name))
+    if os.path.isfile(full_path):
+        try:
+            with open(full_path, 'r') as f:
+                try:
+                    content = json.load(f)
+                except Exception as error:
+                    print(f'Failed to read content in {file_name} (Error: {error})')
+        except Exception as error:
+            print(f'Failed to open file {file_name} (Error: {error})')
+
+    return content 
+
+
+def clean_configs(node_type:str, configs:dict)->dict:
+    """
+    Based on the node_type remove un-need configurations
+    :args:
+        node_type:str
+        configs:dict - configurations
+    :return:
+        cleaned configs
+    """
+    configs['general']['NODE_TYPE']['value'] = node_type
+    if node_type == 'master':
+        configs['general']['NODE_TYPE']['value'] = 'ledger'
+    elif node_type == 'operator':
+        configs['networking']['ANYLOG_SERVER_PORT']['default'] = 32148
+        configs['networking']['ANYLOG_REST_PORT']['default'] = 32149
+    elif node_type == 'publisher':
+        configs['networking']['ANYLOG_SERVER_PORT']['default'] = 32248
+        configs['networking']['ANYLOG_REST_PORT']['default'] = 32249
+    elif node_type == 'query':
+        configs['networking']['ANYLOG_SERVER_PORT']['default'] = 32348
+        configs['networking']['ANYLOG_REST_PORT']['default'] = 32349
+
+    for section in configs:
+        if section == 'networking' and node_type in ['master', 'query']:
+            configs[section]['ANYLOG_BROKER_PORT']['enable'] = False
+        elif section == 'database' and node_type in ['master', 'publisher', 'query', 'standalone-publisher']:
+            for param in configs[section]:
+                if param == 'SYSTEM_QUERY' and node_type == 'query':
+                    configs[section][param]['default'] = 'true'
+                elif 'NOSQL' in param or param == 'AUTOCOMMIT':
+                    configs[section][param]['enable'] = False
+        elif section == 'operator' and node_type in ['master', 'publisher', 'query', 'standalone-publisher']:
+            for param in configs[section]:
+                configs[section][param]['enable'] = False
+        elif section == 'publisher' and node_type in ['master', 'operator', 'query', 'standalone']:
+            for param in configs[section]:
+                configs[section][param]['enable'] = False
+        elif section == 'mqtt' and node_type in ['master', 'query']:
+            for param in configs[section]:
+                configs[section][param]['enable'] = False
+        elif section == 'advanced settings' and node_type in ['master', 'query']:
+            for param in configs[section]:
+                if param != 'DEPLOY_LOCAL_SCRIPT':
+                    configs[section][param]['enable'] = False
+
+    return configs
+
+
+def print_questions(configs:dict)->bool:
+    """
+    Whether or not to print question
+    :args:
+        configs:dict - configuration
+    :param:
+        status:bool
+    :return:
+        if one or more of the config params is enabled return True, else returns False
+    """
+    for param in configs:
+        if configs[param]['enable'] is True:
+            return True
+    return False
+
+
+def prepare_mqtt_params(configs:dict, db_name:str, port:int, user:str, password:str)->dict:
+    """
+    update the default MQTT parameters to match information already provided by the user.
+    :args:
+        configs:dict - (preset) MQTT configurations
+        db_name:str - default logical database
+        port:int - AnyLog broker port
+        user:str - authentication user
+        password:str - authentication user
+    :return:
+        (updated) configs
+    """
+    configs['MQTT_DBMS']['default'] = db_name
+    # if local broker port is set, then update configs accordingly
+    if port != "":
+        configs['MQTT_PORT']['default'] = port
+        configs['MQTT_BROKER']['default'] = 'local'
+        configs['MQTT_USER']['default'] = user
+        configs['MQTT_PASSWD']['default'] = password
+
+    return configs
