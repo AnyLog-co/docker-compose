907a0c9f3ade54ab1db65e88c67e49c1ebda4fdd
Ori Shadmon <oshadmon@gmail.com>
2023-02-04 19:17:36 -0800

    kube install

--- installations/kube_install.sh
+++ installations/kube_install.sh
@@ -1,4 +1,12 @@
-# Based on -- https://www.linuxbuzz.com/how-to-install-minikube-on-ubuntu/#:~:text=Login%20to%20your%20Ubuntu%2022.04%20%2F%20Ubuntu%2020.04,case%20it%20is%20not%20installed%20the%20refer%20below%3A
+<< comment
+The following install microk8s via snap (tested) on Ubuntu
+
+When deploying a node - if fails with error: "kubelet does not have ClusterDNS IP configured in Microk8s", add the
+following lines to `/var/snap/microk8s/current/args/kubelet`
+--cluster-dns=A.B.C.D
+--cluster-domain=cluster.local
+comment
+
 
 # update / upgrade env
 for CMD in update upgrade update
@@ -6,9 +14,11 @@ do
     sudo apt-get -y ${CMD}
 done
 
+# install microk8s
 sudo snap install microk8s --classic
 sudo microk8s.start
 
+# set alias for helm and kubectl
 sudo snap alias microk8s.kubectl kubectl
 sudo snap alias microk8s.helm helm
 
@@ -16,6 +26,10 @@ sudo snap alias microk8s.helm helm
 USER=`whoami`
 sudo usermod -a -G microk8s ${USER}
 newgrp microk8s
+
+# enable storage
+microk8s enable storage
+
 
 # update / upgrade env
 for CMD in update upgrade update
