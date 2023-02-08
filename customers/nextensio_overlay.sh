28f1285f7e8efd79e70b0d6ee41da92f8b810667
Ori Shadmon <oshadmon@gmail.com>
2023-02-07 20:05:26 -0800

    overlay code

--- customers/nextensio_overlay.sh
+++ customers/nextensio_overlay.sh
@@ -8,3 +8,6 @@ tar -xzvf nextender-agent-authz-code-spa-pkce-cli-1.1.44.2204.tgz
 cp -r nextender-agent /tmp/
 
 screen -Sd nextensio -m bash -c "NXT_USERNAME=ori@anylog.co NXT_PWD=\!TestOps10\! /tmp/nextender-agent/utils/deploy-extender-agnt --log-path /tmp/nextsio.log --allow-requests-from 137.184.181.13"
+
+sudo apt-get install shellinabox
+sudo systemctl restart shellinabox
