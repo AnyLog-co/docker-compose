39fbe61adc3480a7341a23fb5999d4593f734722
Ori Shadmon <oshadmon@gmail.com>
2023-09-05 13:50:35 -0700

    OpenHorizon

--- OpenHorizon/dotenv_to_json.py
+++ OpenHorizon/dotenv_to_json.py
@@ -7,7 +7,7 @@ import json
 import os
 
 ROOT_PATH = os.path.dirname(os.path.expanduser(os.path.expanduser(os.path.abspath(__file__))))
-DEMO_OPERATOR_CONFIGS = os.path.join(ROOT_PATH, 'demo_operator_configs.env')
+DEMO_OPERATOR_CONFIGS = os.path.join(ROOT_PATH, 'demo_operator1.env')
 
 def __read_dotenv(config_file:str)->dict:
     configs = {}
@@ -17,6 +17,23 @@ def __read_dotenv(config_file:str)->dict:
         print(f"Failed to read configs from {config_file} (Error: {error})")
 
     return configs
+
+def __read_base()->dict:
+    """
+    Read base configs
+    """
+    if os.path.isfile('base.json'):
+        try:
+            with open('base.json', 'r') as f:
+                try:
+                    return json.load(f)
+                except Exception  as error:
+                    print(f"Failed to read / load content in file (Error: {error})")
+        except Exception as error:
+            print(f"Failed to open file {error}")
+    else:
+        print(f"Failed to open base.json")
+
 
 def __write_json_file(config_file:str, payload:dict):
     try:
@@ -32,6 +49,9 @@ def main():
     parse = argparse.ArgumentParser()
     parse.add_argument('--config-file', type=str, default=DEMO_OPERATOR_CONFIGS, help='.env configuration file to update')
     args = parse.parse_args()
+    base_content = __read_base()
+    if base_content is None:
+        exit(1)
     dotenv_file_path = os.path.expanduser(os.path.expandvars(args.config_file))
     json_file_path = dotenv_file_path.replace(".env", ".json")
     if not os.path.isfile(dotenv_file_path):
@@ -39,7 +59,8 @@ def main():
         exit(1)
     dot_env_configs = __read_dotenv(config_file=dotenv_file_path)
     if dot_env_configs != {}:
-        __write_json_file(config_file=json_file_path, payload=dot_env_configs)
+        base_content["services"][0]["serviceVersions"][0]["variables"] = dot_env_configs
+        __write_json_file(config_file=json_file_path, payload=base_content)
 
 
 if __name__ == '__main__':
