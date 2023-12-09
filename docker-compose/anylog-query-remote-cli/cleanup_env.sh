38fc65d382026a28510dd2a0e6d6acb38803f841
Ori Shadmon <oshadmon@gmail.com>
2023-12-09 15:50:31 -0800

    in progress - port instead of network

--- docker-compose/anylog-query-remote-cli/cleanup_env.sh
+++ docker-compose/anylog-query-remote-cli/cleanup_env.sh
@@ -1,4 +1,4 @@
 export ANYLOG_SERVER_PORT=`cat anylog_configs.env | grep "ANYLOG_REST_PORT" | awk -F "=" '{print $2}'`
 export ANYLOG_REST_PORT=`cat anylog_configs.env | grep "ANYLOG_SERVER_PORT" |  awk -F "=" '{print $2}'`
 
-docker-compose up -d
+docker-compose down -v
