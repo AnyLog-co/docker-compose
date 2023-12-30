8299370533937e2410104d14fc65b6b39dc53b01
Ori Shadmon <oshadmon@gmail.com>
2023-12-29 16:52:03 -0800

    run.sh fix

--- run.sh
+++ run.sh
@@ -58,11 +58,11 @@ else
 fi
 
 
-export ANYLOG_SERVER_PORT=`cat anylog_configs.env | grep "ANYLOG_REST_PORT" | awk -F "=" '{print $2}'`
-export ANYLOG_REST_PORT=`cat anylog_configs.env | grep "ANYLOG_SERVER_PORT" |  awk -F "=" '{print $2}'`
+export ANYLOG_SERVER_PORT=$(cat anylog_configs.env | grep "ANYLOG_SERVER_PORT" | awk -F "=" '{print $2}')
+export ANYLOG_REST_PORT=$(cat anylog_configs.env | grep "ANYLOG_REST_PORT" |  awk -F "=" '{print $2}')
 export ANYLOG_BROKER_PORT=""
 if [[ ${TRAINING} == false ]] && [[ ! $(grep "ANYLOG_BROKER_PORT" advance_configs.env |  awk -F "=" '{print $2}')   = '""' ]] ; then # if ANYLOG_BROKER_PORT exists
-    export ANYLOG_BROKER_PORT=`grep "ANYLOG_BROKER_PORT" advance_configs.env |  awk -F "=" '{print $2}'`
+    export ANYLOG_BROKER_PORT=$(cat anylog_configs.env | grep "ANYLOG_BROKER_PORT" | awk -F "=" '{print $2}')
 fi
 
 
