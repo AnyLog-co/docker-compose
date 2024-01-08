a40a49bf2a180b02b6e1163417a55011936d3857
Ori Shadmon <oshadmon@gmail.com>
2024-01-08 14:35:02 -0800

    volume code

---
+++ kubernetes/secret.sh
@@ -0,0 +1,5 @@
+kubectl create secret docker-registry imagepullsecret \
+    --docker-server=docker.io \
+    --docker-username=oshadmon \
+    --docker-password=docker4AnyLog! \
+    --docker-email=oshadmon@gmail.com
--- run.sh
+++ run.sh
@@ -3,16 +3,18 @@
 # Start / Stop Docker such that AnyLog connects to specific ports, as opposed to using a generic bridge connection
 # Sample Calls:
 #  - help: bash run.sh help
-#  - Start: bash run.sh NODETYPE up [--training]
-#  - Stop: bash run.sh {NODETYPE} down [--training] [--volume] [--rmi]
+#  - Start: bash run.sh helm NODETYPE up || bash run.sh docker NODETYPE up [--training]
+#  - Stop: bash run.sh helm {NODETYPE} down [--volume] || bash run.sh docker {NODETYPE} down [--training] [--volume] [--rmi]
 
-NODETYPE=$1
-DOCKERCMD=$2
+DEPLOYMENT_TYPE=$1
+NODETYPE=$2
+DOCKERCMD=$3
 VOLUME=false
 IMAGE=false
 TRAINING=false
 
-if [[ ${NODETYPE} == help ]] ; then
+
+if [[ ${DEPLOYMENT_TYPE} == help ]] ; then
   printf """Start / Stop Docker such that AnyLog connects to specific ports, as opposed to using a generic bridge connection
   Sample Calls:
   \t- Start: bash run.sh NODETYPE up [--training]
@@ -20,9 +22,7 @@ if [[ ${NODETYPE} == help ]] ; then
   """
   exit 0
 fi
-
 ROOT_PATH=$(dirname $(readlink -f "$0"))
-
 
 if [[ ${DOCKERCMD} == down ]] ; then
   while [[ $# -gt 0 ]] ; do
@@ -43,62 +43,75 @@ if [[ ${DOCKERCMD} == down ]] ; then
   done
 fi
 
-if [[ ${TRAINING} == true ]] ; then
-  if [[ ${NODETYPE} == master ]] || [[ ${NODETYPE} == operator ]] || [[ ${NODETYPE} == query ]] ; then
-    cd ${ROOT_PATH}/training/anylog-${NODETYPE} || (echo "Failed to cd into anylog-${NODETYPE} dir" && exit 1)
+
+if [[ ${DEPLOYMENT_TYPE} == docker ]] ; then
+  if [[ ${TRAINING} == true ]] ; then
+    if [[ ${NODETYPE} == master ]] || [[ ${NODETYPE} == operator ]] || [[ ${NODETYPE} == query ]] ; then
+      cd ${ROOT_PATH}/training/anylog-${NODETYPE} || (echo "Failed to cd into anylog-${NODETYPE} dir" && exit 1)
+    else
+      echo "Invalid deployment type: ${NODETYPE}"
+      exit 1
+    fi
+  elif [[ ${NODETYPE} == master ]] || [[ ${NODETYPE} == operator ]] || [[ ${NODETYPE} == query ]]  || [[ ${NODETYPE} == publisher ]] || [[ ${NODETYPE} == query-remote-cli ]] ; then
+    cd ${ROOT_PATH}/docker-compose/anylog-${NODETYPE} || (echo "Failed to cd into anylog-${NODETYPE} dir" && exit 1)
   else
     echo "Invalid deployment type: ${NODETYPE}"
     exit 1
   fi
-elif [[ ${NODETYPE} == master ]] || [[ ${NODETYPE} == operator ]] || [[ ${NODETYPE} == query ]]  || [[ ${NODETYPE} == publisher ]] || [[ ${NODETYPE} == query-remote-cli ]] ; then
-  cd ${ROOT_PATH}/docker-compose/anylog-${NODETYPE} || (echo "Failed to cd into anylog-${NODETYPE} dir" && exit 1)
-else
-  echo "Invalid deployment type: ${NODETYPE}"
-  exit 1
-fi
 
+  export ANYLOG_SERVER_PORT=$(cat anylog_configs.env | grep "ANYLOG_SERVER_PORT" | awk -F "=" '{print $2}')
+  export ANYLOG_REST_PORT=$(cat anylog_configs.env | grep "ANYLOG_REST_PORT" |  awk -F "=" '{print $2}')
+  export ANYLOG_BROKER_PORT=""
 
-export ANYLOG_SERVER_PORT=$(cat anylog_configs.env | grep "ANYLOG_SERVER_PORT" | awk -F "=" '{print $2}')
-export ANYLOG_REST_PORT=$(cat anylog_configs.env | grep "ANYLOG_REST_PORT" |  awk -F "=" '{print $2}')
-export ANYLOG_BROKER_PORT=""
-if [[ ${TRAINING} == false ]] && [[ ! $(grep "ANYLOG_BROKER_PORT" advance_configs.env |  awk -F "=" '{print $2}')   = '""' ]] ; then # if ANYLOG_BROKER_PORT exists
+  if [[ ${TRAINING} == false ]] && [[ ! $(grep "ANYLOG_BROKER_PORT" advance_configs.env |  awk -F "=" '{print $2}')   = '""' ]] ; then # if ANYLOG_BROKER_PORT exists
     export ANYLOG_BROKER_PORT=$(cat anylog_configs.env | grep "ANYLOG_BROKER_PORT" | awk -F "=" '{print $2}')
-fi
-
-
-if [[ ${DOCKERCMD} == up ]] ; then
-  if [[ ! -z ${ANYLOG_BROKER_PORT} ]] ; then # if ANYLOG_BROKER_PORT exists
-    docker-compose -f docker-compose-broker.yml up -d
-  else
-    docker-compose up -d
   fi
-elif [[ ${DOCKERCMD} == down ]] ; then
-  if [[ ${VOLUME} == true ]] && [[ ${IMAGE} == true ]] ; then
+
+  if [[ ${DOCKERCMD} == up ]] ; then
     if [[ ! -z ${ANYLOG_BROKER_PORT} ]] ; then # if ANYLOG_BROKER_PORT exists
-    docker-compose -f docker-compose-broker.yml down -v --rmi all
+      docker-compose -f docker-compose-broker.yml up -d
     else
-      docker-compose down -v --rmi all
+      docker-compose up -d
     fi
-  elif [[ ${VOLUME} == true ]] && [[ ${IMAGE} == false ]] ; then
-    if [[ ! -z ${ANYLOG_BROKER_PORT} ]] ; then # if ANYLOG_BROKER_PORT exists
-    docker-compose -f docker-compose-broker.yml down -v
+  elif [[ ${DOCKERCMD} == down ]] ; then
+    if [[ ${VOLUME} == true ]] && [[ ${IMAGE} == true ]] ; then
+      if [[ ! -z ${ANYLOG_BROKER_PORT} ]] ; then # if ANYLOG_BROKER_PORT exists
+      docker-compose -f docker-compose-broker.yml down -v --rmi all
+      else
+        docker-compose down -v --rmi all
+      fi
+    elif [[ ${VOLUME} == true ]] && [[ ${IMAGE} == false ]] ; then
+      if [[ ! -z ${ANYLOG_BROKER_PORT} ]] ; then # if ANYLOG_BROKER_PORT exists
+      docker-compose -f docker-compose-broker.yml down -v
+      else
+        docker-compose down -v
+      fi
+    elif [[ ${VOLUME} == false ]] && [[ ${IMAGE} == true ]] ; then
+      if [[ ! -z ${ANYLOG_BROKER_PORT} ]] ; then # if ANYLOG_BROKER_PORT exists
+      docker-compose -f docker-compose-broker.yml down --rmi all
+      else
+        docker-compose down --rmi all
+      fi
     else
-      docker-compose down -v
-    fi
-  elif [[ ${VOLUME} == false ]] && [[ ${IMAGE} == true ]] ; then
-    if [[ ! -z ${ANYLOG_BROKER_PORT} ]] ; then # if ANYLOG_BROKER_PORT exists
-    docker-compose -f docker-compose-broker.yml down --rmi all
-    else
-      docker-compose down --rmi all
+      if [[ ! -z ${ANYLOG_BROKER_PORT} ]] ; then # if ANYLOG_BROKER_PORT exists
+        docker-compose -f docker-compose-broker.yml down
+      else
+        docker-compose down
+      fi
     fi
   else
-    if [[ ! -z ${ANYLOG_BROKER_PORT} ]] ; then # if ANYLOG_BROKER_PORT exists
-      docker-compose -f docker-compose-broker.yml down
-    else
-      docker-compose down
+    echo Invalid Userinput ${DOCKERCMD}
+    exit 1
+  fi
+elif [[ ${DEPLOYMENT_TYPE} == helm ]] ; then
+  NODE_NAME=$(grep "pod_name" ${ROOT_PATH}/kubernetes/configs/anylog_${NODETYPE}.yaml | awk -F ":" '{print $2}')
+  if [[ ${DOCKERCMD} == up ]] ; then
+    helm install ${ROOT_PATH}/kubernetes/anylog-node-volume-1.22.3.tgz -f ${ROOT_PATH}/kubernetes/configs/anylog_${NODETYPE}.yaml --name-template ${NODE_NAME}-volume
+    helm install ${ROOT_PATH}/kubernetes/anylog-node-1.22.3.tgz -f ${ROOT_PATH}/kubernetes/configs/anylog_${NODETYPE}.yaml --name-template ${NODE_NAME}
+  elif [[ ${DOCKERCMD} == down ]] ; then
+    helm uninstall ${NODE_NAME}
+    if [[ ${VOLUME} == down ]] ; then
+      helm uninstall ${NODE_NAME}-volume
     fi
   fi
-else
-  echo Invalid Userinput ${DOCKERCMD}
-  exit 1
 fi
