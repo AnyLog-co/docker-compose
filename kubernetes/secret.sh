30f9fd9b579956922ed9220828249da7ef3ff85e
Ori Shadmon <oshadmon@gmail.com>
2024-04-19 18:07:35 -0700

    working license
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- kubernetes/secret.sh
+++ kubernetes/secret.sh
@@ -3,6 +3,7 @@
 # command: bash secret.sh ${DOCKER_KEY}
 
 PASSWORD=$1
+echo ${PASSWORD}
 kubectl create secret docker-registry imagepullsecret \
     --docker-server=docker.io \
     --docker-username=anyloguser \
