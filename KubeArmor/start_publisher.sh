95177755d2da595b750b6e76f672ad6e9fbc1c0c
Ori Shadmon <oshadmon@gmail.com>
2024-09-07 15:00:49 -0700

    version
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- KubeArmor/start_publisher.sh
+++ KubeArmor/start_publisher.sh
@@ -7,3 +7,5 @@ kubectl create secret docker-registry imagepullsecret \
   --docker-password=docker4AnyLog! \
   --docker-email=ori@anylog.co
 
+bash deploy_node.sh packaage anylog_publisher.yaml  10.128.0.8
+bash deploy_node.sh start anylog_publisher.yaml  10.128.0.8
