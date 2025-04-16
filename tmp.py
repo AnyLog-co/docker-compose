a12cf4b91d29b202bb5f606ebbc82c71cee732f0
Ori Shadmon <oshadmon@gmail.com>
2025-04-15 19:55:23 -0700

    README
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- tmp.py
+++
@@ -1,4 +0,0 @@
-output = ""
-for i in ["a_current", "a_n_voltage", "b_current", "b_n_voltage", "c_current", "c_n_voltage"]:
-	output += f"min({i}) as min_{i}, avg({i}) as avg_{i}, max({i}) as max_{i},"
-print(output)
