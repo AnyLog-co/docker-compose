dea0df3150a7146b09322c3b27dbf37ec681ab6d
Ori Shadmon <oshadmon@gmail.com>
2025-10-03 21:36:30 -0700

    reboot
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- run_demo.sh
+++ run_demo.sh
@@ -4,10 +4,11 @@ USER_INPUT=$1
 export MY_DIR=`pwd`
 cd $HOME/docker-compose
 if [[ "${USER_INPUT}" == "run" ]] ; then
-  make up ANYLOG_TYPE=demo-node TAG=latest
+  make up ANYLOG_TYPE=demo-node
   docker logs -f anylog-demo-node
 else
   make down ANYLOG_TYPE=demo-node
   docker volume rm `docker volume ls | grep -v nebula | awk -F " " '{print $2}'`
+  sudo reboot
 fi
 cd ${MY_DIR}
