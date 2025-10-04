d942955153bc7b6aa0e240a9c95c320841a72ad6
Ori Shadmon <oshadmon@gmail.com>
2025-10-03 19:07:10 -0700

    rename / deployment script
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

---
+++ run_demo.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+USER_INPUT=$1
+
+export MY_DIR=`pwd`
+cd $HOME/docker-compose
+if [[ "${USER_INPUT}" == "run" ]] ; then
+  make up ANYLOG_TYPE=demo-node
+  docker log -f demo-node
+else ;
+  make clean-vols ANYLOG_TYPE=demo-node
+fi
+cd ${MY_DIR}
