94e1efd893000d59a7924cefd6437c2b3774c3a6
Ori Shadmon <oshadmon@gmail.com>
2025-10-03 19:26:14 -0700

    minor chages for support
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 # Default values
 export IS_MANUAL ?= false
-export TAG ?= 1.4.2508-beta10
+export TAG ?= latest
 
 ifeq ($(IS_MANUAL), true)
 	export ANYLOG_TYPE ?= generic
--- run_demo.sh
+++ run_demo.sh
@@ -4,7 +4,7 @@ USER_INPUT=$1
 export MY_DIR=`pwd`
 cd $HOME/docker-compose
 if [[ "${USER_INPUT}" == "run" ]] ; then
-  make up ANYLOG_TYPE=demo-node
+  make up ANYLOG_TYPE=demo-node TAG=latest
   docker log -f demo-node
 else
   make clean-vols ANYLOG_TYPE=demo-node
